4b1770dc4d1a3580f9689575ccbbebed8b6ca46d
aadriellyy <limaadrielly214@gmail.com>
2024-12-04 01:37:05 -0300

    refatoração do código: expandindo caso de teste 1

--- velha.cpp
+++ velha.cpp
@@ -13,13 +13,45 @@
  *  Descrever o que a funcao faz
  */ 
 
-bool vencedorX( int velha[3][3] ){
+bool venceuLinha(int velha[3][3]){
 	for(int j = 0; j < 3; j++){
 		if(velha[j][0] == 1 && velha[j][1] == 1 && velha[j][2] == 1){
 			return true;
 		}
 	}
 	return false;
+}
+
+bool venceuColuna(int velha[3][3]){
+	for(int j = 0; j < 3; j++){
+		if(velha[0][j] == 1 && velha[1][j] == 1 && velha[2][j] == 1){
+			return true;
+		}
+	}
+	return false;
+}
+
+bool venceuDiagonalPrincipal(int velha[3][3]){
+	if(velha[0][0] == 1 && velha[1][1] == 1 && velha[2][2] == 1){
+		return true;
+	}
+	return false;
+}
+
+bool venceuDiagonalSecundaria(int velha[3][3]){
+	if(velha[0][2] == 1 && velha[1][1] == 1 && velha[2][0] == 1){
+		return true;
+	}
+	return false;
+}
+
+bool vencedorX( int velha[3][3] ){
+	if(venceuLinha(velha) == true || venceuColuna(velha) == true || 
+		venceuDiagonalPrincipal(velha) == true || venceuDiagonalSecundaria(velha) == true){
+			return true;
+	}
+	return false;
+
 }
 
 int VerificaVelha( int velha[3][3] )
