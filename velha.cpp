6fe357291c88362f92fe6020a8ba7f4f443989c2
aadriellyy <limaadrielly214@gmail.com>
2024-12-04 01:56:59 -0300

    add: expandindo casos de teste para o teste 2

--- velha.cpp
+++ velha.cpp
@@ -27,7 +27,10 @@ bool venceuLinha(int velha[3][3]){
 bool venceuColuna(int velha[3][3]){
 	for(int j = 0; j < 3; j++){
 		if(velha[0][j] == 1 && velha[1][j] == 1 && velha[2][j] == 1){
-			return true;
+			return 1;
+		}
+		else if(velha[0][j] == 0 && velha[1][j] == 0 && velha[2][j] == 0){
+			return 0;
 		}
 	}
 	return false;
@@ -35,14 +38,20 @@ bool venceuColuna(int velha[3][3]){
 
 bool venceuDiagonalPrincipal(int velha[3][3]){
 	if(velha[0][0] == 1 && velha[1][1] == 1 && velha[2][2] == 1){
-		return true;
+		return 1;
+	}
+	else if(velha[0][0] == 0 && velha[1][1] == 0 && velha[2][2] == 0){
+		return 0;
 	}
 	return false;
 }
 
 bool venceuDiagonalSecundaria(int velha[3][3]){
 	if(velha[0][2] == 1 && velha[1][1] == 1 && velha[2][0] == 1){
-		return true;
+		return 1;
+	}
+	else if(velha[0][2] == 0 && velha[1][1] == 0 && velha[2][0] == 0){
+		return 0;
 	}
 	return false;
 }
@@ -57,7 +66,8 @@ bool vencedorX( int velha[3][3] ){
 }
 
 bool vencedorO( int velha[3][3] ){
-	if(venceuLinha(velha) == 0){
+	if(venceuLinha(velha) == 0 || venceuColuna(velha) == 0 || 
+		venceuDiagonalPrincipal(velha) == 0 || venceuDiagonalSecundaria(velha) == 0){
 		return true;
 	}
 	return false;
